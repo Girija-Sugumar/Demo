89e475fbc14c3d4748c74af936228cfd63122a8d
Girija-Sugumar <133954729+Girija-Sugumar@users.noreply.github.com>
2024-02-22 19:01:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello, Practising to push docker image to ecr!')
+  res.send('Hello')
 })
 
 app.get('/fun', (req, res) => {
