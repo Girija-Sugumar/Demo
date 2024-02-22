421df09b12d26329dce67f74a00547f3b2a60bf8
Girija-Sugumar <girija.sukumar@softsuave.org>
2024-02-22 13:45:46 +0000

    changed

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/api/', (req, res) => {
   res.send('Hello, Practising to push docker image to ecr!')
 })
 
-app.get('/fun', (req, res) => {
+app.get('/api/fun', (req, res) => {
   res.send('Application deployed')
 })
 
